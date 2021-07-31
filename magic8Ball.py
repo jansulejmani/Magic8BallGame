7e5c3d2234d27f6da1adbcac13fae8353b373412
Jan Sulejmani <jansulejmani@Jans-Mac.local>
2021-07-31 20:10:53 +0200

    changes

--- magic8Ball.py
+++ magic8Ball.py
@@ -1,27 +1,16 @@
 import random
 
-def getAnswer(answerNumber):
-	print("Ask a question: ")
-	userAnswer = input()
-	if answerNumber == 1:
-		return "It is certain"
-	elif answerNumber == 2:
-		return "It is decidedly so"
-	elif answerNumber == 3:
-		return "Yes"
-	elif answerNumber == 4:
-		return "Reply hazy try again"
-	elif answerNumber == 5:
-		return "Ask again later"
-	elif answerNumber == 6:
-		return "Concentrate and ask again"
-	elif answerNumber == 7:
-		return "My reply is no"
-	elif answerNumber == 8:
-		return "Outlook not so good"
-	elif answerNumber == 9:
-		return "Very doubtful"
+messages = ["It is certain",
+"It is decidedly so",
+"Yes definitely",
+"Reply hazy try again",
+"Ask again later",
+"Concentrate and ask again",
+"My reply is no",
+"Outlook not so good",
+"Very doubtful"]
 
-r = random.randint(1,9)
-fortune = getAnswer(r)
-print(fortune)
+print("Enter a simple question: ")
+user = input()
+
+print(messages[random.randint(0, len(messages) - 1)])
